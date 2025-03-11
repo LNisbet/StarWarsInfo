b10e736a8212e4b6832c10eec03e556cb10ce39a
LNisbet <luke.nisbet@stfc.ac.uk>
2025-03-11 19:03:51 +0000

    Refactored GetFilmInfo to remove static methods from the class

--- StarWarsInfo.Domain/Methods/GetFilmInfo.cs
+++ StarWarsInfo.Domain/Methods/GetFilmInfo.cs
@@ -2,6 +2,7 @@
 using StarWarsInfo.Core.Entities;
 using StarWarsInfo.Core.Entities.Interfaces;
 using StarWarsInfo.Core.Exceptions;
+using StarWarsInfo.Domain.Methods.HelperMethods;
 using StarWarsInfo.Domain.Methods.Interfaces;
 using System.Collections.ObjectModel;
 
@@ -20,45 +21,9 @@ namespace StarWarsInfo.Domain.Methods
 
             foreach (var film in films)
             {
-                filmCollection.Add(BuildFilmInfoFromFilm(film, personRepo, planetRepo));
+                filmCollection.Add(BuildEntities.BuildFilmInfoFromFilm(film, personRepo, planetRepo));
             }
             return filmCollection;
-        }
-
-        private static FilmInfo BuildFilmInfoFromFilm(Film film, IRepository<Person> personRepo, IRepository<Planet> planetRepo)
-        {
-            List<ICharacterInfo> characters = [];
-
-            foreach (var character in film.Characters)
-            {
-                characters.Add(BuildCharecterFromUrl(character, personRepo, planetRepo));
-            }
-
-            List<string> planets = [];
-
-            foreach (var planet in film.Planets)
-            {
-                planets.Add(GetPlanetNameFromUrl(planet, planetRepo));
-            }
-
-            return new FilmInfo(int.Parse(film.EpisodeId), film.Title, DateOnly.Parse(film.ReleaseDate), film.OpeningCrawl, planets, characters);
-        }
-
-        private static CharacterInfo BuildCharecterFromUrl(string url, IRepository<Person> personRepo, IRepository<Planet> planetRepo)
-        {
-            var charecter = personRepo.GetById(GetIdFromURL(url));
-            return new CharacterInfo(charecter.Name, planetRepo.GetById(GetIdFromURL(charecter.Homeworld)).Name);
-        }
-
-        private static string GetPlanetNameFromUrl(string url, IRepository<Planet> planetRepo)
-        {
-            return planetRepo.GetById(GetIdFromURL(url)).Name;
-        }
-
-        private static int GetIdFromURL(string url)
-        {
-            string[] parts = url.Split('/');
-            return int.Parse(parts[^2]); //Second to last part
         }
     }
 }
---
+++ StarWarsInfo.Domain/Methods/HelperMethods/BuildEntities.cs
@@ -0,0 +1,50 @@
+﻿using StarWarsApiCSharp;
+using StarWarsInfo.Core.Entities.Interfaces;
+using StarWarsInfo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWarsInfo.Domain.Methods.HelperMethods
+{
+    internal class BuildEntities
+    {
+        internal static FilmInfo BuildFilmInfoFromFilm(Film film, IRepository<Person> personRepo, IRepository<Planet> planetRepo)
+        {
+            List<ICharacterInfo> characters = [];
+
+            foreach (var character in film.Characters)
+            {
+                characters.Add(BuildCharecterFromUrl(character, personRepo, planetRepo));
+            }
+
+            List<string> planets = [];
+
+            foreach (var planet in film.Planets)
+            {
+                planets.Add(GetPlanetNameFromUrl(planet, planetRepo));
+            }
+
+            return new FilmInfo(int.Parse(film.EpisodeId), film.Title, DateOnly.Parse(film.ReleaseDate), film.OpeningCrawl, planets, characters);
+        }
+
+        internal static CharacterInfo BuildCharecterFromUrl(string url, IRepository<Person> personRepo, IRepository<Planet> planetRepo)
+        {
+            var charecter = personRepo.GetById(GetIdFromURL(url));
+            return new CharacterInfo(charecter.Name, planetRepo.GetById(GetIdFromURL(charecter.Homeworld)).Name);
+        }
+
+        internal static string GetPlanetNameFromUrl(string url, IRepository<Planet> planetRepo)
+        {
+            return planetRepo.GetById(GetIdFromURL(url)).Name;
+        }
+
+        private static int GetIdFromURL(string url)
+        {
+            string[] parts = url.Split('/');
+            return int.Parse(parts[^2]); //Second to last part
+        }
+    }
+}
